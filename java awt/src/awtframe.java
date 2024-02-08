be59a684ade6f350c3cd47dbc4687f5c2a77daf7
Nasir <mohdnasir80092@gmail.com>
2024-02-08 23:13:16 +0530

    first commit

---
+++ java awt/src/awtframe.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class awtframe {
+    public awtframe(){
+        Frame frame = new Frame("gui");
+        frame.setSize (400,800);
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e){
+                frame.dispose();
+            }
+        });
+    }
+
+    public static void main(String[] args){
+        awtframe obj1 = new awtframe();
+    }
+}
